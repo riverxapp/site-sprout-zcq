60b7d1a5eb38d3e33978cc29be18b53560aaf4f3
Chirag Dodiya <hi@chirag.co>
2026-04-25 20:34:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
-            <Toaster />
           </ThemeProvider>
       </body>
     </html>
